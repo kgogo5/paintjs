870780fcfba84f6c695ba1bb02a577b8a0ae34a5
https://github.com/kgogo5/ <kgogo9@gmail.com>
2020-03-30 00:48:03 +0900

    200330 commit

---
+++ canvas.js
@@ -0,0 +1,42 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(e) {
+    const x = e.offsetX;
+    const y = e.offsetY;
+
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown() {
+    painting = ture;
+}
+
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
